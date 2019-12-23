a55c6ddd38741ad37ff262c74d6595528fa7b9ca
2444070359 <2444070359@qq.com>
2019-12-23 20:42:24 +0800

    cxzc

---
+++ holle/src/cn/blb/Hello.java
@@ -0,0 +1,18 @@
+package cn.blb;
+
+public class Hello {
+	public void show(){
+		System.out.println("开发功能报表");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello");
+		System.out.println("1111");
+		System.out.println("2222");
+		System.out.println("545644");
+		System.out.println("分支切换");
+
+	}
+
+}
---
+++ holle/src/cn/blb/a1.java
@@ -0,0 +1,10 @@
+package cn.blb;
+
+public class a1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("132");
+	}
+
+}
